e113b40079f53b6e079dadf406678b6f85d78bd2
xsanm <kkurowski.kk@gmail.com>
2021-03-10 22:20:40 +0100

    directory structure

---
+++ lab00/zad2/lib.c
@@ -0,0 +1,132 @@
+#define  _POSIX_C_SOURCE 200809L
+
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+#include "lib.h"
+
+
+struct block *create_blocks(int number_of_blocks) {
+    if (number_of_blocks < 0) {
+        return NULL;
+    }
+    struct block *res = calloc(number_of_blocks, sizeof(struct block));
+    return res;
+}
+
+struct pair *create_pairs(int number_of_pairs) {
+    if (number_of_pairs < 0) {
+        return NULL;
+    }
+    struct pair *res = calloc(number_of_pairs, sizeof(struct pair));
+    return res;
+}
+
+void merge(struct pair *pairs, int number_of_pairs) {
+    for(int i = 0; i < number_of_pairs; i++) {
+        FILE * fp_a = fopen(pairs[i].a_adress, "r");
+        FILE * fp_b = fopen(pairs[i].b_adress, "r");
+        if (fp_a == NULL || fp_b == NULL) {
+            printf("ERROR WHILE READING pair: %s:%s\n", pairs[i].a_adress, pairs[i].b_adress);
+            return;
+        }
+
+        char merged_name[16] = "merged";
+        char nr[6];
+        sprintf(nr, "%d", i);
+        strcat(merged_name, nr);
+        strcat(merged_name, ".txt");
+        pairs[i].merged_adress = malloc(strlen(merged_name) * sizeof(char));
+        strcpy(pairs[i].merged_adress, merged_name);
+
+        FILE *f = fopen(merged_name, "w+");
+        char * line = NULL;
+        size_t len = 0;
+        int a = 1, b = 1;
+        int rows = 0;
+        while(a || b) {
+            if(a) {
+                if(getline(&line, &len, fp_a) != -1) {
+                    fprintf(f, "%s", line);
+                    rows++;
+                } else {
+                    a = 0;
+                }
+            }
+            if(b) {
+                if(getline(&line, &len, fp_b) != -1) {
+                    fprintf(f, "%s", line);
+                    rows++;
+                } else {
+                    b = 0;
+                }
+            }
+        }
+        pairs[i].rows = rows;
+        
+        fclose(fp_a);
+        fclose(fp_b);
+        fclose(f);
+    }
+}
+
+int add_block(struct block *main_arr, char *temp_file, int i, int rows_num) {
+    int lines = 0;
+    size_t len = 0;
+    FILE *f = fopen(temp_file, "r");
+    char **rows = calloc(rows_num, sizeof(char *));
+    for(int i = 0; i < rows_num; i++) {
+        rows[i] = NULL;
+    }
+
+    while(getline(&rows[lines], &len, f) != -1) {
+        lines++;
+    }
+
+    main_arr[i].rows = rows;
+    main_arr[i].number_of_rows = lines;
+
+    fclose(f);
+    return i;
+}
+
+int rows_in_block(struct block *main_arr, int id) {
+    return main_arr[id].number_of_rows;
+}
+
+void del_block(struct block *main_arr, int id) {
+    for(int i = 0; i < main_arr[id].number_of_rows; i++) {
+        free(main_arr[id].rows[i]);
+        main_arr[id].rows[i] = NULL;
+    }
+    free(main_arr[id].rows);
+    main_arr[id].rows = NULL;
+}
+
+void del_row_from_block(struct block *main_arr, int block_id, int row_id) {
+    free(main_arr[block_id].rows[row_id]);
+    main_arr[block_id].rows[row_id] = NULL;
+}
+
+void display_table(struct block *main_arr, int blocks) {
+    for(int i = 0; i < blocks; i++) {
+        printf("\nBLOCK NR %d:\n", i);
+        if(main_arr[i].rows == NULL) {
+            puts("DELETED");
+            continue;
+        }
+        for(int j = 0; j < main_arr[i].number_of_rows; j++) {
+            if(main_arr[i].rows[j] != NULL) printf("%s", main_arr[i].rows[j]);
+            else puts("DELETED");
+        }
+    }
+}
+
+void display_pairs(struct pair *pairs, int number_of_pairs) {
+    for(int i = 0; i < number_of_pairs; i++) {
+        printf("Pair %d: %s <> %s\n", i, pairs[i].a_adress, pairs[i].b_adress);
+    }
+}
---
+++ lab00/zad3/main.c
@@ -0,0 +1,228 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include <sys/times.h>
+#include <time.h>
+#include <unistd.h>
+#include <dlfcn.h>
+
+#define  _GNU_SOURCE
+#include <stdio.h>
+
+#include "lib.h"
+
+double calculate_time_clocks(clock_t start, clock_t end) {
+    return (double) (end - start) / CLOCKS_PER_SEC;
+}
+
+double calculate_time_tics(clock_t start, clock_t end) {
+    return (double) (end - start) / sysconf(_SC_CLK_TCK);
+}
+
+char *generate_random_line() {
+    const int SIZE = (rand() % 16) + 16;
+    char *base = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz";
+    size_t base_len = strlen(base);
+
+    char *res = (char *) malloc(SIZE * sizeof(char));
+
+    for (int i = 0; i < SIZE - 2; i++) {
+        res[i] = base[rand() % base_len];
+    }
+    res[SIZE - 2] = '\n';
+    res[SIZE - 1] = '\0';
+    return res;
+}
+
+void generate_file(char *f_name, int rows_number) {
+    FILE *f = fopen(f_name, "w+");
+    for(int i = 0; i < rows_number; i++) {
+        fprintf(f, "%s", generate_random_line());
+    }
+    fclose(f);
+}
+
+void run_time_test(int blocks, int rows_number) {
+    struct tms operation_time[8]; //usr and sys
+    clock_t operation_time_real[8]; //real
+
+    struct block *table = create_blocks(blocks);
+    struct pair *pairs = create_pairs(blocks);
+
+    for(int i = 0; i < blocks; i++) {
+        char f_name[32] = "a_test_";
+        char nr[16];
+        sprintf(nr, "%d", i);
+        strcat(f_name, nr);
+        strcat(f_name, ".txt");
+        
+        generate_file(f_name, rows_number);
+        pairs[i].a_adress = calloc(strlen(f_name), sizeof(char));
+        strcpy(pairs[i].a_adress, f_name);
+
+        f_name[0] = 'b';
+        generate_file(f_name, rows_number);
+        pairs[i].b_adress = calloc(strlen(f_name), sizeof(char));
+        strcpy(pairs[i].b_adress, f_name);
+    }
+
+    times(&operation_time[0]);
+    operation_time_real[0] = clock();
+
+    merge(pairs, blocks);
+
+    times(&operation_time[1]);
+    operation_time_real[1] = clock();
+
+    for(int j = 0; j < blocks; j++) {
+        add_block(table, pairs[j].merged_adress, j, pairs[j].rows);
+    }
+
+    times(&operation_time[2]);
+    operation_time_real[2] = clock();
+
+    for(int j = 0; j < blocks; j++) {
+        del_block(table, j);
+    }
+
+    times(&operation_time[3]);
+    operation_time_real[3] = clock();
+
+    for(int k = 0; k <= 10; k++) {
+        for(int j = 0; j < blocks; j++) {
+            add_block(table, pairs[j].merged_adress, j, pairs[j].rows);
+        }
+        for(int j = 0; j < blocks; j++) {
+            del_block(table, j);
+        }   
+    }
+    times(&operation_time[4]);
+    operation_time_real[4] = clock();
+
+    puts("              REAl        USER        SYSTEM");
+    printf("MERGE       %lf", calculate_time_clocks(operation_time_real[0], operation_time_real[1]));
+    printf("    %lf", calculate_time_tics(operation_time[0].tms_utime, operation_time[1].tms_utime));
+    printf("    %lf\n", calculate_time_tics(operation_time[0].tms_stime, operation_time[1].tms_stime));
+
+    printf("SAVE        %lf", calculate_time_clocks(operation_time_real[1], operation_time_real[2]));
+    printf("    %lf", calculate_time_tics(operation_time[1].tms_utime, operation_time[2].tms_utime));
+    printf("    %lf\n", calculate_time_tics(operation_time[1].tms_stime, operation_time[2].tms_stime));
+
+    printf("DEL         %lf", calculate_time_clocks(operation_time_real[2], operation_time_real[3]));
+    printf("    %lf", calculate_time_tics(operation_time[2].tms_utime, operation_time[3].tms_utime));
+    printf("    %lf\n", calculate_time_tics(operation_time[2].tms_stime, operation_time[3].tms_stime));
+
+    printf("SAVE-DEL    %lf", calculate_time_clocks(operation_time_real[3], operation_time_real[4]));
+    printf("    %lf", calculate_time_tics(operation_time[3].tms_utime, operation_time[4].tms_utime));
+    printf("    %lf\n", calculate_time_tics(operation_time[3].tms_stime, operation_time[4].tms_stime));
+
+}
+
+int main(int argc, char **argv) {
+    srand((unsigned int) time(NULL));
+
+    if(argc >= 2 && strcmp(argv[1], "tests") == 0) {
+        puts("\n### SMALL TESTS V1 ### ");
+        puts("5 FILES, 10 ROWS");
+        run_time_test(5, 10);
+
+        puts("\n### SMALL TESTS V2 ### ");
+        puts("5 FILES, 100 ROWS");
+        run_time_test(5, 100);
+
+        puts("\n### MEDIUM TESTS V1 ### ");
+        puts("200 FILES, 100 ROWS");
+        run_time_test(200, 100);
+
+        puts("\n### MEDIUM TESTS V2 ### ");
+        puts("200 FILES, 400 ROWS");
+        run_time_test(200, 400);
+
+        puts("\n### LARGE TESTS V1 ### ");
+        puts("1000 FILES, 100 ROWS");
+        run_time_test(1000, 100);
+
+        puts("\n### LARGE TESTS V2 ### ");
+        puts("1000 FILES, 600 ROWS");
+        run_time_test(1000, 600);
+    
+        return 0;
+    }
+
+    struct block *table = NULL;
+    int blocks;
+    struct pair *pairs = NULL;
+    
+
+    if(argc <= 4) {
+        puts("NOT ENOUGH ARGUMENTS");
+        return 1;
+    } else if(strcmp(argv[1], "create_table")) {
+        puts("YOU HAVE TO CREATE TABLE");
+        return 1;
+    } else if(strcmp(argv[3], "merge_files")) {
+        puts("YOU HAVE TO MERGE FILES");
+        return 1;
+    }
+
+    blocks = strtol(argv[2], NULL, 10);
+
+    table = create_blocks(blocks);
+    pairs = create_pairs(blocks);
+    
+    int i = 4;
+    for(int j = 0; j < blocks; j++) {
+        char *pair = argv[i++];
+        int position = (int)(strchr(pair, ':') - pair);
+        
+        pairs[j].a_adress =  calloc((position + 1), sizeof(char));
+        pairs[j].b_adress =  calloc((strlen(pair) - position + 1), sizeof(char));
+        sscanf(pair, "%[^:]:%s", pairs[j].a_adress, pairs[j].b_adress);
+
+        //strncpy(pairs[j].a_adress, pair, position);
+        //strncpy(pairs[j].b_adress, pair + position + 1, (strlen(pair) - position));
+    }
+
+    //display_pairs(pairs, blocks);
+
+    merge(pairs, blocks);
+
+    for(int j = 0; j < blocks; j++) {
+        add_block(table, pairs[j].merged_adress, j, pairs[j].rows);
+    }
+
+    puts("\n### INITAL BLOCKS ###");
+    display_table(table, blocks);
+
+    while(i < argc) {
+        if(strcmp(argv[i], "remove_block") == 0) {
+            int block_id = strtol(argv[i + 1], NULL, 10);
+            if(block_id < 0 || block_id >= blocks) {
+                puts("WRONG BLOCK ID");
+            } else {
+                del_block(table, block_id);
+            }
+           i++;
+        } else if(strcmp(argv[i], "remove_row") == 0) {
+            int block_id = strtol(argv[i + 1], NULL, 10);
+            int row_id = strtol(argv[i + 2], NULL, 10);
+            if(block_id < 0 || block_id >= blocks) {
+                puts("WRONG BLOCK ID");
+            } else if(row_id < 0 || row_id >= rows_in_block(table, block_id)){
+                puts("WRONG ROW ID");
+            } else {
+                del_row_from_block(table, block_id, row_id);
+            }
+            i += 2;
+        } else {
+            puts("invalid instruction");
+        }
+        i++;
+    }
+
+    puts("\n### FINAL BLOCKS ###");
+    display_table(table, blocks);
+    puts("");
+    
+    return 0;
+}
