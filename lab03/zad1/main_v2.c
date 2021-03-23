fc7975f81b8d15f3e9d44f57b3797416aa821b5d
xsanm <kkurowski.kk@gmail.com>
2021-03-23 16:23:25 +0100

    zad1 without static variable

---
+++ lab03/zad1/main_v2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(int argc, int **argv) {
+
+    if(argc != 2) {
+        puts("WRONG NUMBER OF ARGUMENTS");
+        return 1;
+    }
+
+    int pocess_number = strtol((const char*) argv[1], NULL, 10);
+
+    pid_t child_pid = getpid();
+    printf("Parent-process pid: %d\n\n", getpid());
+
+    for(int id = 0; id <= pocess_number; id++) {
+        if(child_pid == 0) {
+            printf("Child-process nr: %d, pid: %d, ppid %d\n", id, getpid(), getppid());
+            return 0;
+        } else {
+            child_pid = fork();
+        }
+    }
+
+    return 0;
+}
+
+
+
