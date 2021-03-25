740f0c68d5e2d6d265c951a52429f3894bc44ce8
xsanm <kkurowski.kk@gmail.com>
2021-03-25 01:37:44 +0100

    zad3 refactor, still problems with parent dirs

--- lab03/zad3/main.c
+++ lab03/zad3/main.c
@@ -15,28 +15,21 @@ int check_if_exist(char *path, char *pattern) {
     //return 1;
 
     FILE *f = fopen(path, "r");
-    //printf("%s\n", path);
     if(f == NULL) {
         return -1;
     }
 
     char buff[BUFF_SIZE];
-    //printf("%d\n", fread(buff, sizeof (char), BUFF_SIZE, f));
     while (fread(buff, sizeof (char), BUFF_SIZE, f) > 0) {
-        //printf("%s", buff);
         if(strstr(buff, pattern) != NULL) return 1;
         fseek(f, -strlen(pattern), SEEK_CUR);
     }
-
     return 0;
 }
 
 void check_dir(char *path, char *pattern, int depth) {
-
     //printf("PATH %s\t PID %d    PPID %d\n", path, getpid(), getppid());
-
     DIR* cdir = opendir(path);
-
     if(cdir == NULL) {
         //show errno error
         exit(0);
@@ -44,39 +37,29 @@ void check_dir(char *path, char *pattern, int depth) {
 
     struct dirent* d = readdir(cdir);
     while(d != NULL) {
-
         if(depth < MAX_DEPTH && DT_DIR == d->d_type && strcmp(".", d->d_name) && strcmp("..", d->d_name)) {
             //printf("Directory: %s\n", d->d_name);
             if(fork() == 0) {
                 PROCESS_CNT++;
-                //printf("Im child: %d, my parent is: %d AND now what? \n ", getpid(), getppid());
-                char path2[512] = "";
-                strcat(path2, path);
-                strcat(path2, "/");
-                strcat(path2, d->d_name);
-                //printf("%s\n", path);
-                closedir(cdir);
-                check_dir(path2, pattern, depth + 1);
 
+                char path2[512] = "";
+                snprintf(path2, sizeof (path2), "%s/%s", path, d->d_name);
+                closedir(cdir);
+
+                check_dir(path2, pattern, depth + 1);
                 exit(0);
             }
             //jeszcze musi byc kropka!
         } else if(DT_REG == d->d_type && strlen(d->d_name) >= 3 && strcmp("txt", d->d_name + strlen(d->d_name) - 3) == 0) {
-            //printf("File (to search): %s \n", d->d_name);
-
             char file_path[512] = "";
-            strcat(file_path, path);
-            strcat(file_path, "/");
-            strcat(file_path, d->d_name);
+            snprintf(file_path, sizeof (file_path), "%s/%s", path, d->d_name);
 
             if(check_if_exist(file_path, pattern) > 0) {
                 printf("%s\tPID %d\t PPID %d\n", file_path + strlen(BASE_PATH) + 1, getpid(), getppid());
             }
-            //printf("Im process: %d, my parent is: %d\n", getpid(), getppid());
         }
         d = readdir(cdir);
     }
-
     closedir(cdir);
 }
 
@@ -90,7 +73,6 @@ int main(int argc, char **argv) {
     MAX_DEPTH = strtol(argv[3], NULL, 10);
 
     check_dir(BASE_PATH, argv[2], 0);
-
 
     for(int i = 0; i < PROCESS_CNT; i++) {
         wait(NULL);
