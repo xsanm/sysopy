16789ae31cee7580e1f3b1ea4ff697a251abe65b
xsan <kkurowski.kk@gmail.com>
2021-06-04 13:28:23 +0200

    connect clients to server

--- lab10/zad1/client.c
+++ lab10/zad1/client.c
@@ -1,12 +1,56 @@
 #include "shared.h"
 
+char *my_name;
+int server_socket;
+
+void connect_to_server(char *type, char *address) {
+    if(strcmp(type, "local") == 0) {
+        //connect local socket
+
+        if ((server_socket = socket(AF_UNIX, SOCK_STREAM, 0)) <= 0) {
+            perror("socket failed");
+            exit(1);
+        }
+        struct sockaddr_un sa;
+        sa.sun_family = AF_UNIX;
+        strcpy(sa.sun_path, address);
+        if (connect(server_socket, (struct sockaddr *) &sa, sizeof(sa)) < 0) {
+            perror("connect local failed");
+            exit(1);
+        }
+
+    } else if(strcmp(type, "network") == 0) {
+        //connect to network socket
+
+        int port = atoi(address);
+        if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) <= 0) {
+            perror("socket failed");
+            exit(1);
+        }
+        struct sockaddr_in sa;
+        sa.sin_family = AF_INET;
+        sa.sin_port = htons(port);
+        //sa.sin_addr.s_addr = INADDR_ANY;
+        if (connect(server_socket, (struct sockaddr *) &sa, sizeof(sa)) < 0) {
+            perror("connect network failed");
+            exit(1);
+        }
+
+    } else {
+        exit(1);
+    }
+}
+
 int main(int argc, char **argv) {
     if(argc != 4) {
         puts("WRONG NUMBER OF ARGUMENTS");
         return 1;
     }
+    my_name = argv[1];
 
     printf("CLIENT START\n");
+
+    connect_to_server(argv[2], argv[3]);
 
     return 0;
 }
